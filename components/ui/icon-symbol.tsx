a40aa951795125d9c96d7ba14a0743274eea0a4c
khancc <truongkhanga@gmail.com>
2026-04-15 18:26:43 +0700

    chore(icons): map new custom sf symbols for tab bar

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -18,6 +18,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'shield.fill': 'security',
+  'leaf.fill': 'eco',
+  'person.fill': 'person',
 } as IconMapping;
 
 /**
